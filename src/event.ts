f960fb27a1d8e88620e93555d262ba3f59663538
Cesar Melo <cavmelo@gmail.com>
2019-11-25 09:51:31 -0400

    Setup Package

--- src/event.ts
+++ src/event.ts
@@ -1,33 +1,35 @@
-export interface iEvent{
-  set(key:string,records:[number]):void
-  get(key:string):[number]|undefined
-  dump():Object
+export interface IEvent {
+  set(key: string, records: [number]): void;
+  get(key: string): [number] | undefined;
+  dump(): object;
 }
 
-export class Event implements iEvent{
-  private logs:Map<string,[number]>
-  constructor(){
-    this.logs = new Map()
-//    this.logs = new Object()
+export class Event implements IEvent {
+  private logs: Map<string, [number]>;
+  constructor() {
+    this.logs = new Map();
+    //    this.logs = new Object()
   }
-  set(key:string, records:[number]):void{
-      this.logs.set(key,records)
+  public set(key: string, records: [number]): void {
+    this.logs.set(key, records);
   }
-  get(key:string):[number]|undefined{
-      return this.logs.get(key)
+  public get(key: string): [number] | undefined {
+    return this.logs.get(key);
   }
 
-  push(key:string, value:number):void{
-    let isExist = key in this.logs
-    if (isExist){
-      const records:[number]|undefined = this.logs.get(key)
-      if (records !== undefined)
-          records.push(value)
-    }else{
-      this.logs.set(key,[value])
+  public push(key: string, value: number): void {
+    const isExist = key in this.logs;
+    if (isExist) {
+      const records: [number] | undefined = this.logs.get(key);
+      if (records !== undefined) {
+        records.push(value);
+      }
+    } else {
+      this.logs.set(key, [value]);
     }
   }
-  dump():Object{
+
+  public dump(): object {
     return this.logs;
   }
 }
